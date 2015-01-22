75b630fdd48d79c6bd681b5d5fdfb9510e0ba7f5
fosiperalta <fosiperalta@gmail.com>
2015-01-22 17:32:29 +0100

    5?

--- db/migrate/20141205162008_change_data_type_for_meeting_date.rb
+++ db/migrate/20141205162008_change_data_type_for_meeting_date.rb
@@ -1,8 +1,14 @@
 class ChangeDataTypeForMeetingDate < ActiveRecord::Migration
   def self.up
-    change_table :meetings do |t|
-      t.change :date, :datetime
-    end
+    create_table 'meetings' do |t|
+	  	t.string 'title'
+	  	t.datetime 'date'
+	  	t.string 'country'
+	  	t.string 'city'
+	  	t.text 'direction'
+	  	t.string 'creator'
+	  	t.timestamps
+	  end
   end
 
   def down
